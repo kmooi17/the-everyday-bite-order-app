06e578df074ba0fc30504ff90fef3c5366c9c5c3
kane <kane.mooi@proquoai.com>
2022-01-23 20:45:37 +0200

    added images, email functionality & busy with order types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { StepperOrientation, STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
-import { Validators, FormBuilder, FormControl } from '@angular/forms';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Observable, map } from 'rxjs';
+
+declare let Email: any;
 
 @Component({
   selector: 'app-root',
@@ -16,30 +18,68 @@ import { Observable, map } from 'rxjs';
   ]
 })
 export class AppComponent {
+  #smtpConfig = {
+    SecureToken: 'aea96db4-a53b-41e4-ad77-f5c22a50b171'
+  };
+
+  cookies: Cookie[] = [
+    { flavour: 'Astronomical Swirl', price: [75, 115] },
+    { flavour: 'Confetti Bomb', price: [70, 110] },
+    { flavour: 'Candy Rain', price: [70, 110] },
+    { flavour: 'Cappa-Cookie', price: [60, 100] },
+    { flavour: 'Surgary-Cin', price: [60, 100] },
+    { flavour: 'Mental Mint Bombs', price: [60, 100] },
+    { flavour: 'Chocolate Typhoon', price: [60, 100] },
+    { flavour: 'Delicate Surprise', price: [90, 130] },
+    { flavour: 'Nostalgic Delight', price: [75, 115] },
+    { flavour: 'Pean-nutty & nice', price: [60, 100] },
+    { flavour: 'Queen of Hearts', price: [60, 100] },
+    { flavour: 'Back to the Beach', price: [75, 115] },
+    { flavour: 'Mad Hatters Tea Party', price: [60, 100] },
+    { flavour: 'Lovers Paradise', price: [60, 100] },
+    { flavour: 'Twixed Dreams', price: [70, 110] }
+  ];
+
+  deliveryPrices: DeliveryPrice[] = [
+    { where: 'Johannesburg', amount: 50 },
+    { where: 'Pretoria', amount: 75 }
+  ];
+
+  OrderTypeEnum = OrderTypeEnum;
+  orderTypes: OrderType[] = [
+    { id: OrderTypeEnum.CHOOSE, value: 'I want to choose' },
+    { id: OrderTypeEnum.VARIETY, value: 'Variety Box of 12' },
+    { id: OrderTypeEnum.CUSTOM, value: 'I want something custom' }
+  ];
+
+  //#region Contact Details FormGroup
   name = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
   cell = new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]+')]);
   delivery = new FormControl(false);
+  where = new FormControl('Johannesburg');
   address = new FormControl('');
 
   minDate: Date;
   maxDate: Date;
-  orderNeeded = new FormControl(undefined, [Validators.required]);
+  orderNeeded: FormControl;
 
-  contactDetailsFG = this._formBuilder.group({
-    name: this.name,
-    email: this.email,
-    cell: this.cell,
-    delivery: this.delivery,
-    address: this.address,
-    orderNeeded: this.orderNeeded
+  contactDetailsFG: FormGroup
+  //#endregion
+
+  //#region Place Order FormGroup
+  orderType = new FormControl(OrderTypeEnum.CHOOSE, [Validators.required]);
+  customOrder = new FormControl('');
+  qty = new FormControl('0');
+  flavour = new FormControl('');
+
+  placeOrderFG = this._formBuilder.group({
+    orderType: this.orderType,
+    customOrder: this.customOrder,
   });
+  //#endregion
 
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-
-  thirdFormGroup = this._formBuilder.group({
+  confirmFG = this._formBuilder.group({
     thirdCtrl: ['', Validators.required],
   });
 
@@ -56,6 +96,17 @@ export class AppComponent {
 
     this.minDate = new Date(currentYear, currentMonth, currentDay + 2);
     this.maxDate = new Date(currentYear, 11, 31);
+
+    this.orderNeeded = new FormControl(this.minDate, [Validators.required]);
+    this.contactDetailsFG = this._formBuilder.group({
+      name: this.name,
+      email: this.email,
+      cell: this.cell,
+      delivery: this.delivery,
+      where: this.where,
+      address: this.address,
+      orderNeeded: this.orderNeeded
+    });
   }
 
   getCellErrorMessage() {
@@ -78,7 +129,69 @@ export class AppComponent {
     this.delivery.value ? this.address.addValidators(Validators.required) : this.address.removeValidators(Validators.required);
   }
 
+  updateOrderType() {
+    setTimeout(() => {
+
+      switch (this.orderType.value) {
+        case OrderTypeEnum.VARIETY:
+
+          this.customOrder.removeValidators(Validators.required);
+          this.customOrder.updateValueAndValidity();
+          break;
+        case OrderTypeEnum.CUSTOM:
+          this.customOrder.addValidators(Validators.required);
+          this.customOrder.updateValueAndValidity();
+
+          break;
+        default:
+          // OrderTypeEnum.CHOOSE
+
+          this.customOrder.removeValidators(Validators.required);
+          this.customOrder.updateValueAndValidity();
+          break;
+      }
+
+      console.log(this.placeOrderFG);
+
+    });
+  }
+
   test() {
     console.log(this.contactDetailsFG);
+
+    // Email.send({
+    //   SecureToken: this.#smtpConfig.SecureToken,
+    //   To: 'the.everyday.bite.sa@gmail.com',
+    //   From: 'kanem410@gmail.com',
+    //   Subject: 'NEW ORDER: This is another test',
+    //   Body: `<h1>New order from...</h1><br/><p>Kane Mooi</><br/><p>Need to make this look pretty with order details :)</p>`
+    // }).then((message: any) => { console.log(message); });
   }
+}
+
+type Cookie = {
+  flavour: string,
+  price: [number, number],
+  quantity?: 6 | 12
+}
+
+// TODO: Add Regular Customer once they have ordered 5 times
+type DeliveryPrice = {
+  where: string,
+  amount: number
+}
+
+type OrderType = {
+  id: number,
+  value: String
+}
+
+enum OrderTypeEnum {
+  CHOOSE,
+  VARIETY,
+  CUSTOM
+}
+
+class OrderDetails {
+  // orderNumber: number;
 }
